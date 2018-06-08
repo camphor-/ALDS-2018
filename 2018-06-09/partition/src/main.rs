f788097d606ab603e87b3e0a2691b3d0b6eeaa11
Yusuke Sangenya <longinus.eva@gmail.com>
2018-06-08 20:21:09 +0900

    読みやすくした

--- 2018-06-09/partition/src/main.rs
+++ 2018-06-09/partition/src/main.rs
@@ -1,28 +1,16 @@
-fn swap<T: Clone>(series: &mut Vec<T>, i: usize, j: usize) {
-    let tmp = series[i].clone();
-    series[i] = series[j].clone();
-    series[j] = tmp;
-}
+fn read_lines() -> Vec<String> {
+    use std::io::Read;
 
-fn partition<T: Ord + Clone>(series: &mut Vec<T>, p: usize, r: usize) -> usize {
-    let x = series[r].clone();
-    let mut i = p;
-    for j in p..r {
-        if series[j] <= x {
-            swap(series, i, j);
-            i += 1;
-        }
-    }
-    swap(series, i, r);
-    return i;
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+    return buf.lines().map(|e| e.to_string()).collect::<Vec<_>>();
 }
 
 fn main() {
-    let input = read_string();
-    let input = input.lines().collect::<Vec<_>>();
+    let input = read_lines();
     let n: usize = input[0].parse().unwrap();
+    let mut series = input[1].split_whitespace().map(|s| s.parse().unwrap()).collect::<Vec<u32>>();
     let r = n - 1;
-    let mut series = input[1].split_whitespace().map(|e| e.parse().unwrap()).collect::<Vec<u32>>();
 
     let q = partition(&mut series, 0, r);
 
@@ -31,10 +19,21 @@ fn main() {
     println!("{} [{}] {}", small, series[q], large);
 }
 
-fn read_string() -> String {
-    use std::io::Read;
+fn partition<T: Ord + Clone>(series: &mut Vec<T>, p: usize, r: usize) -> usize {
+    let x = series[r].clone();
+    let mut i = (p as i64) - 1;
+    for j in p..r {
+        if series[j] <= x {
+            swap(series, (i + 1) as usize, j);
+            i += 1;
+        }
+    }
+    swap(series, (i + 1) as usize, r);
+    return (i + 1) as usize;
+}
 
-    let mut buf = String::new();
-    std::io::stdin().read_to_string(&mut buf).unwrap();
-    return buf;
+fn swap<T: Clone>(series: &mut Vec<T>, i: usize, j: usize) {
+    let tmp = series[i].clone();
+    series[i] = series[j].clone();
+    series[j] = tmp;
 }
