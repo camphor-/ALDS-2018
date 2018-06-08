8aff9c0b1b29ca577ea80fb8d6df51cb8fe98324
Yusuke Sangenya <longinus.eva@gmail.com>
2018-06-08 18:19:33 +0900

    読みやすくした

--- 2018-06-09/merge_sort/src/main.rs
+++ 2018-06-09/merge_sort/src/main.rs
@@ -1,23 +1,32 @@
-fn read_string() -> String {
+fn read_lines() -> Vec<String> {
     use std::io::Read;
 
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
-    return buf;
+    return buf.lines().map(|e| e.to_string()).collect::<Vec<_>>();
 }
 
 static mut COMP_COUNT: u32 = 0;
 
 fn main() {
-    let input = read_string();
-    let input = input.lines().collect::<Vec<_>>();
+    let input = read_lines();
     let n: usize = input[0].parse().unwrap();
     let mut series = input[1].split_whitespace().map(|s| s.parse().unwrap()).collect::<Vec<u32>>();
+
     merge_sort(&mut series, 0, n);
 
     println!("{}", series.into_iter().map(|e| format!("{}", e)).collect::<Vec<String>>().join(" "));
     unsafe {
         println!("{}", COMP_COUNT);
+    }
+}
+
+fn merge_sort(series: &mut Vec<u32>, left: usize, right: usize) {
+    if left + 1 < right {
+        let mid = (left + right) / 2;
+        merge_sort(series, left, mid);
+        merge_sort(series, mid, right);
+        merge(series, left, mid, right);
     }
 }
 
@@ -40,14 +49,5 @@ fn merge(series: &mut Vec<u32>, left: usize, mid: usize, right: usize) {
             series[i] = ls[li];
             li += 1;
         }
-    }
-}
-
-fn merge_sort(series: &mut Vec<u32>, left: usize, right: usize) {
-    if left + 1 < right {
-        let mid = (left + right) / 2;
-        merge_sort(series, left, mid);
-        merge_sort(series, mid, right);
-        merge(series, left, mid, right);
     }
 }
